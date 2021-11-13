78aed56a1459ad6be509298a36596e14740861b6
zenon-provalidator <zenon@provalidator.com>
2021-11-13 16:39:22 +0900

    loop

--- provalidator_osmosis_bot/provalidator_osmosis_bot.js
+++ provalidator_osmosis_bot/provalidator_osmosis_bot.js
@@ -74,31 +74,31 @@ bot.command('proposal', (ctx) =>{
 		})		
 	}
 })
-//loop
-const botJob = new CronJob(`*/60 * * * * *`, async function () {
-	let latestProposal = func.getLatestProposalNum() //마지막 프로포절 번호 가져오기
-
-	if(latestProposal !== 0 ){
-		let callProposalNum = latestProposal+1
-		let getProposal = func.getProposalFromServer(callProposalNum)
-		
-		if(typeof getProposal === "object"){
-			let msg = `🧪 <b>오스모시스 ($OSMO)</b>\n`
-			msg += `ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n\n`
-			msg += `<b>🗳️새로운 프로포절이 올라왔습니다.</b>\n\n`
-			msg += `#${callProposalNum} ${getProposal.title}\n\n`
-			msg += `📌<a href='https://www.mintscan.io/cosmos/proposals/${callProposalNum}'>https://www.mintscan.io/cosmos/proposals/${callProposalNum}</a>\n\n`
-//			msg += `<b>프로밸리와 $OSMO 스테이킹 하세요❤</b>\n\n`
-			msg += `ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n`
-			msg += `<b>프로밸리(<a href='https://provalidator.com'>Provalidator</a>) 검증인 만듦</b>`				
-			bot.telegram.sendMessage(process.env.PROPOSAL_ALERT_ROOM_ID, msg,{ parse_mode: 'HTML', disable_web_page_preview : true})
-		} else if(getProposal === 203){
-			logger.debug(`${callProposalNum} proposal is not found`)
-		} else{
-			logger.error(`server error`)
-		}
-	}else{
-		logger.error(`latestProposal is 0`)
-	}
-	
-}).start()
+//loop ProvalidatorOsmoBot에서 돌리니므로 주석
+//const botJob = new CronJob(`*/60 * * * * *`, async function () {
+//	let latestProposal = func.getLatestProposalNum() //마지막 프로포절 번호 가져오기
+//
+//	if(latestProposal !== 0 ){
+//		let callProposalNum = latestProposal+1
+//		let getProposal = func.getProposalFromServer(callProposalNum)
+//		
+//		if(typeof getProposal === "object"){
+//			let msg = `🧪 <b>오스모시스 ($OSMO)</b>\n`
+//			msg += `ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n\n`
+//			msg += `<b>🗳️새로운 프로포절이 올라왔습니다.</b>\n\n`
+//			msg += `#${callProposalNum} ${getProposal.title}\n\n`
+//			msg += `📌<a href='https://www.mintscan.io/cosmos/proposals/${callProposalNum}'>https://www.mintscan.io/cosmos/proposals/${callProposalNum}</a>\n\n`
+////			msg += `<b>프로밸리와 $OSMO 스테이킹 하세요❤</b>\n\n`
+//			msg += `ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n`
+//			msg += `<b>프로밸리(<a href='https://provalidator.com'>Provalidator</a>) 검증인 만듦</b>`				
+//			bot.telegram.sendMessage(process.env.PROPOSAL_ALERT_ROOM_ID, msg,{ parse_mode: 'HTML', disable_web_page_preview : true})
+//		} else if(getProposal === 203){
+//			logger.debug(`${callProposalNum} proposal is not found`)
+//		} else{
+//			logger.error(`server error`)
+//		}
+//	}else{
+//		logger.error(`latestProposal is 0`)
+//	}
+//	
+//}).start()
